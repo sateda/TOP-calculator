1a53a9f225cf57e1640cbdbd373d6693ca6b898e
sateda <7441698+sateda@users.noreply.github.com>
2025-05-04 20:39:55 +0200

    Added four functions for basic math

---
+++ script.js
@@ -0,0 +1,25 @@
+function add(a, b) {
+    return a + b;    
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+const resultAdd = add(1,2);
+const resultSubtract = subtract(2,1);
+const resultMultiply = multiply(2,2);
+const resultDivide = divide(4,2);
+
+console.log(resultAdd);
+console.log(resultSubtract);
+console.log(resultMultiply);
+console.log(resultDivide);
