20b5e92325962dae27ea32bad8387dc921006110
sateda <7441698+sateda@users.noreply.github.com>
2025-05-05 14:52:48 +0200

    Added functionality to the buttons and update the display

--- script.js
+++ script.js
@@ -20,9 +20,7 @@ let operator;
 
 const result = operate(1 ,2, "divide");
 
-function operate(numberA, numberB, operator) {
-    console.log(operator);
-    
+function operate(numberA, numberB, operator) {   
     switch(operator) {
         case "add": return add(numberA, numberB)
         case "subtract": return subtract(numberA, numberB);
@@ -32,5 +30,15 @@ function operate(numberA, numberB, operator) {
     }
 }
 
+// Evaluate input and refresh the display
+function refreshDisplay(output) {
+    const display = document.querySelector("#display");
+    display.setAttribute("value", output);
+}
 
-console.log(result);
+// Add event handler to all the buttons and invoke refresh function
+const input_buttons = document.querySelectorAll(".input_button");
+input_buttons.forEach((input_button) => {
+    input_button.addEventListener("click", event => refreshDisplay(event.target.value));
+});
+
