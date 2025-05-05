1b5ece0f4d39fcf9f2b77f523d8355f877c8c5a7
sateda <7441698+sateda@users.noreply.github.com>
2025-05-05 20:51:31 +0200

    changed function calculatorAction to work with strings

--- script.js
+++ script.js
@@ -39,6 +39,38 @@ function refreshDisplay(output) {
 // Add event handler to all the buttons and invoke refresh function
 const input_buttons = document.querySelectorAll(".input_button");
 input_buttons.forEach((input_button) => {
-    input_button.addEventListener("click", event => refreshDisplay(event.target.value));
+    input_button.addEventListener("click", event => calculatorAction(event.target.value));
 });
 
+// calculator flow function
+let newNumber = "";
+let numberA = "";
+let numberB = "";
+let possibleNumbers = new Set(["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]);
+
+function calculatorAction(input) {
+    if(possibleNumbers.has(input)) {
+        newNumber = newNumber + input;
+        refreshDisplay(newNumber);
+    }
+}
+
+
+// let counter = 1;
+
+// function calculatorAction(input) {
+//     let numberA = 0;
+//     let numberB = 0;
+    
+//     // Convert input to an number if possible
+//     const inputNumber = Number(input);
+
+//     if(Number.isInteger(inputNumber) === true) {
+//         let newNumber = inputNumber * counter;
+//         refreshDisplay(newNumber);
+        
+//         counter++;
+//     }
+
+
+// }
