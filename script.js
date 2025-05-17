430f573cb3e490720cd7af5d3549a51090658955
sateda <7441698+sateda@users.noreply.github.com>
2025-05-17 14:48:56 +0200

    Added all the logic, calculator works

--- script.js
+++ script.js
@@ -21,11 +21,14 @@ let operator;
 const result = operate(1 ,2, "divide");
 
 function operate(numberA, numberB, operator) {   
+    let a = Number(numberA);
+    let b = Number(numberB);
+    
     switch(operator) {
-        case "add": return add(numberA, numberB)
-        case "subtract": return subtract(numberA, numberB);
-        case "multiply": return multiply(numberA, numberB);
-        case "divide": return divide(numberA, numberB);
+        case "add": return add(a, b)
+        case "subtract": return subtract(a, b);
+        case "multiply": return multiply(a, b);
+        case "divide": return divide(a, b);
         default: console.log("Error: operator unknown");
     }
 }
@@ -42,22 +45,79 @@ input_buttons.forEach((input_button) => {
     input_button.addEventListener("click", event => calculatorAction(event.target.value));
 });
 
-// calculator flow function
+// calculator variables
 let display = "";
-let numberA = "";
-let numberB = "";
+let displayResult = 0;
+let numberA = "x";
+let numberB = "y";
+let requestedOperation = "";
 let possibleNumbers = new Set(["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]);
+let operation = new Set(["add", "subtract", "multiply", "divide"]);
+
 
 function calculatorAction(input) {    
+    // Clear display and reset numbers if last operation was equal sign
+    if(requestedOperation == "=" && displayResult === 1) {
+        display = "";
+        numberA = "x";
+        numberB = "y";
+        displayResult = 0;
+        requestedOperation = "";
+        refreshDisplay(display);
+        console.log("hello");
+    }
+    
+    // Clear display if previous action was an result
+    if(displayResult === 1) {
+        display = "";
+        refreshDisplay(display);
+        displayResult = 0;
+    }
+    
     // If input is an number add it to the string newNumber
     if(possibleNumbers.has(input)) {
         display = display + input;
         refreshDisplay(display);
     }
 
+    // If an operator was pressed decide what to do
+    if(operation.has(input)) {
+        requestedOperation = input;
+        if(numberA === "x") {
+            numberA = display;
+            display = "";
+            refreshDisplay(display);
+            console.log("Number A is:" + numberA);
+        } else if (numberB === "y") {
+            numberB = display; // set numberB
+            let result = operate(numberA, numberB, requestedOperation); // perform calculation
+            numberA = result;
+            numberB = "y";
+            display = result;
+            displayResult = 1;          
+            refreshDisplay(display);            
+        } else {
+            console.log("ERROR");
+        }
+    }
+
+    // logic based on equal or clear
     switch(input) {
         case "Clear":
             display = "";
+            numberA = "x";
+            numberB = "y";
+            requestedOperation = "";
+            refreshDisplay(display);
+            break;
+        case "=":
+            numberB = display;
+            let result = operate(numberA, numberB, requestedOperation); // perform calculation
+            numberA = result;
+            numberB = "y";
+            display = result;
+            displayResult = 1;
+            requestedOperation = "=";     
             refreshDisplay(display);
             break;
     }
